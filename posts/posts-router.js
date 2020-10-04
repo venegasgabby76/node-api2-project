3539a143439acc7bc8b0b818a25de8ebeafcb4e8
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2020-10-04 14:07:26 -0700

    got my first park running

---
+++ posts/posts-router.js
@@ -0,0 +1,8 @@
+
+
+// When the client makes a `GET` request to `/api/posts`:
+
+
+// When the client makes a `GET` request to `/api/posts/:id`:
+
+// When the client makes a `GET` request to `/api/posts/:id/comments`
