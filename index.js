954f60d8d9bf523e4633aacb0d801c39423ee210
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2020-10-04 14:36:30 -0700

    got all my get requests done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express")
+const express = require("express");
 
-// const postRouter = require("./posts/posts-router")
+const postRouter = require("./posts/posts-router")
 
-const welcomeRouter = require("./welcome/welcome-router.js")
+const welcomeRouter = require("./welcome/welcome-router.js");
 
-const server = express()
-const port = 2727
+const server = express();
+const port = 2727;
 
-server.use(express.json())
-// server.use(postRouter)
-server.use(welcomeRouter)
+server.use(express.json());
+server.use(postRouter)
+server.use(welcomeRouter);
 
 server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,8 +1,63 @@
+const express = require("express");
+const posts = require("../data/db");
 
+const router = express.Router();
 
 // When the client makes a `GET` request to `/api/posts`:
-
+router.get("api/posts", (req, res) => {
+  posts
+    .find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "The posts information could not be retrieved.",
+      });
+    });
+});
 
 // When the client makes a `GET` request to `/api/posts/:id`:
+router.get("/api/posts/:id", (req, res) => {
+  posts
+    .findById(req.params.id)
+    .then((posts) => {
+      if (posts) {
+        res.status(200).json(posts);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be retrieved.",
+      });
+    });
+});
 
 // When the client makes a `GET` request to `/api/posts/:id/comments`
+router.get("/api/posts/:id/comments", (req, res) => {
+  posts
+    .findCommentById(req.params.id)
+    .then((posts) => {
+      if (posts) {
+        res.status(200).json(posts);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "The comments information could not be retrieved.",
+      });
+    });
+});
+
+module.exports = router;
