d37755bef5ba4bbb7d213e2ceadf860777f0a920
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2020-10-04 21:26:51 -0700

    finished the project

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -88,14 +88,14 @@ router.post("/posts", (req, res) => {
 
 // When the client makes a `POST` request to `/api/posts/:id/comments`
 router.post("/posts/:id/comments", (req, res) => {
-  if (!req.body.text) {
+  if (!req.body.text || !req.body.post_id) {
     return res.status(400).json({
       errorMessage: "Please provide text for the comment.",
     });
   }
 
   posts
-    .insertComment(req.body)
+    .insertComment(req.body.post_id)
     .then((comments) => {
       if (comments) {
         res.status(201).json(comments);
@@ -118,8 +118,57 @@ router.post("/posts/:id/comments", (req, res) => {
 
 // When the client makes a `PUT` request to `/api/posts/:id`
 
+router.put("/posts/:id", (req, res) => {
+    if (!req.body.title || !req.body.contents) {
+      return res.status(400).json({
+        message: "The post with the specified ID does not exist."
+      });
+    }
+  
+    posts
+      .update(req.params.id, req.body)
+      .then((post) => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({
+            errorMessage: "Please provide title and contents for the post."
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+            error: "The post information could not be modified.",
+        });
+      });
+  });
+
 // DELETE REQUESTS
 
 // When the client makes a `DELETE` request to `/api/posts/:id`
+
+router.delete("/posts/:id", (req, res) => {
+    posts
+      .remove(req.params.id)
+      .then((count) => {
+        if (count > 0) {
+          res.status(200).json({
+            message: "The post has been removed!",
+          });
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist.",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+            error: "The post could not be removed",
+        });
+      });
+  });
+  
 
 module.exports = router;
